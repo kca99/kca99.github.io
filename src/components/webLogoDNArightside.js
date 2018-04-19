efb779c0886e6d0c3656008c59c3893b32c22c27
kca99 <kca99@sfu.ca>
2018-04-18 22:20:04 -0700

    trying to push to root

---
+++ src/components/webLogoDNArightside.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './webLogo.css';
+
+class Svg extends Component {
+  render() {
+    return (
+      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 84.57 263.08" height="100%">
+
+        <path className="cls-1" d="M54.42,204.36c-38.11,35.27-38.2,67.11-38.2,67.11" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M54.42,75.51l1.83,1.71" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-2" d="M59.91,80.75c20.45,20.25,38.84,47,28.77,76.37-3,8.85-8.43,17.16-13.94,24.7a153.36,153.36,0,0,1-16.65,19" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M56.25,202.64l-1.83,1.71" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M16.25,8.4s0.06,31.84,38.17,67.11" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M54.66,204.36l1.82,1.71" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-3" d="M60,209.52c24.66,24.94,30.78,47.41,32.3,57" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M92.62,269c0.17,1.62.18,2.49,0.18,2.49" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M54.66,75.51c-22,20.36-45.91,51.14-33.82,82.39A106.05,106.05,0,0,0,34.2,181.61a156.94,156.94,0,0,0,20.46,22.75" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M92.71,8.4s0,0.87-.17,2.49" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-4" d="M91.79,15.75c-2.16,10.9-9.38,32.61-33.54,56.33" transform="translate(-12.22 -8.39)"/>
+        <path className="cls-1" d="M56.48,73.79l-1.82,1.71" transform="translate(-12.22 -8.39)"/>
+        <g id="lines">
+          <line className="cls-5" x1="42.89" y1="99.11" x2="59.78" y2="99.11"/>
+          <line className="cls-1" x1="15.56" y1="99.11" x2="42.89" y2="99.11"/>
+          <line className="cls-5" x1="42.89" y1="36.11" x2="61.28" y2="36.11"/>
+          <line className="cls-1" x1="15.26" y1="36.11" x2="42.89" y2="36.11"/>
+          <line className="cls-5" x1="42.89" y1="163.97" x2="61.5" y2="163.97"/>
+          <line className="cls-1" x1="15.82" y1="163.97" x2="42.89" y2="163.97"/>
+          <line className="cls-5" x1="44.33" y1="131.54" x2="72.28" y2="131.54"/>
+          <line className="cls-1" x1="5.37" y1="131.54" x2="42.33" y2="131.54"/>
+          <line className="cls-5" x1="42.89" y1="253.61" x2="70.78" y2="253.61"/>
+          <line className="cls-1" x1="4.69" y1="253.61" x2="42.89" y2="253.61"/>
+          <line className="cls-5" x1="42.89" y1="228.83" x2="60.75" y2="228.83"/>
+          <line className="cls-1" x1="14.78" y1="228.83" x2="42.89" y2="228.83"/>
+          <line className="cls-5" x1="42.89" y1="15.11" x2="68.28" y2="15.11"/>
+          <line className="cls-1" x1="5.93" y1="15.11" x2="42.89" y2="15.11"/>
+        </g>
+      </svg>
+    );
+  }
+}
+
+export default Svg;
